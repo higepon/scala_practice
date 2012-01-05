c9b6f1fa07b9d360ccc92dc12c3a44922a279bca
taro <higepon@gmail.com>
2012-01-05 15:25:59 +0900

    First commit

---
+++ src/hello_world/HelloWorld.scala
@@ -0,0 +1,15 @@
+package hello_world
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: taro
+ * Date: 12/01/05
+ * Time: 15:11
+ * To change this template use File | Settings | File Templates.
+ */
+
+object HelloWorld {
+  def main(args: Array[String]) {
+    println("Hello, World")
+  }
+}
